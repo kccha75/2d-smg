f9e97407ab7b3a787c4a48d0829fad0f386afbbc
Keith Chan <kccha75@gmail.com>
2021-05-19 11:40:40 +1000

    added 1d scheme

---
+++ Scheme/fourier_Lu_1d.m
@@ -0,0 +1,25 @@
+% Performs multiplication L*u using Fourier collocation methods
+% L is the spectral operator for the given PDE in the form
+% -u_xx+au_x+bu using
+%
+% Inputs:
+% v - best estimate
+% pde.a
+% pde.b
+% domain.k - wave number
+% domain.N
+%
+% Ouputs:
+% A - L*u
+%
+
+function Lu=fourier_Lu_1d(v,pde,domain)
+
+kx=domain.k(:,1);
+
+a=pde.a;
+b=pde.b;
+
+Lu=-ifft(-kx.^2.*fft(v))+a.*ifft(1i.*kx.*fft(v))+b.*v;
+
+end
