d79666106e2602d6cf726396489407d7a8bc04b4
kccha5 <kccha5@gmail.com>
2021-06-07 11:42:27 +1000

    updated with preconditioner + restart

--- Test/bicgstabtest.m
+++ Test/bicgstabtest.m
@@ -5,7 +5,7 @@
 % Inputs:
 % v - best estimate
 % pde - structure consisting of pde coefficients
-% scheme.k - wave number in x
+% scheme.k - wave number
 % option.operator - operator used to find Lu
 % option.numit - number of iterations
 % option.preconditioner - preconditioner used
@@ -19,80 +19,105 @@
 
 function [v,r]=bicgstabtest(v,pde,domain,option)
 f=pde.f;
-maxit=2000;
+maxit=100;
 
 % Initial residual
 r=pde.f-option.operator(v,pde,domain);
 p=r;
 d=r;
 
-if rms(r)<option.tol
+if rms(r(:))<option.tol
     return
 end
 
-% % Check if preconditioner available
-% if ~isempty(option.preconditioner) && option.prenumit~=0
-%     
-%     pde.f=r;
-%     option.numit=option.prenumit;
-%     p=option.preconditioner(zeros(size(v)),pde,domain,option);
-%     
-% else
-%     
-%     p=r;
-%     
-% end
-
-delta=dot(r(:),d(:));
 
 for i=1:maxit
     
-    q=option.operator(p,pde,domain);
+    % Check if preconditioner available (note single use of preconditioner)
+    % should be K1*K2y=p generally where K=K1*K2
+    if ~isempty(option.preconditioner) && option.prenumit~=0
+    
+        pde.f=p;
+        option.numit=option.prenumit;
+        y=option.preconditioner(zeros(size(v)),pde,domain,option);
+    
+    else
+    
+        y=p;
+    
+    end
+
+    delta=dot(r(:),d(:));
+    
+    q=option.operator(y,pde,domain);
     alpha=delta/dot(d(:),q(:));
     s=r-alpha*q;
     
-    z=option.operator(s,pde,domain);% A*s
+    % Tolerance check
+    if rms(s(:))<option.tol
+        v=v+alpha*p;
+        fprintf('Bi Conjugate Gradient STAB Converged after %d iterations!\n',i);
+        break
+    end
     
-    w=dot(z(:),s(:))/dot(z(:),z(:));
-    v=v+alpha*p+w*s; % estimate of new solution
+    
+% Check if preconditioner available (note single use of preconditioner)
+% should be K1*K2z=s generally where K=K1*K2
+if ~isempty(option.preconditioner) && option.prenumit~=0
+    
+    pde.f=s;
+    option.numit=option.prenumit;
+    z=option.preconditioner(zeros(size(v)),pde,domain,option);
+    
+    t=option.operator(z,pde,domain);% A*s
+    
+    pde.f=t;
+    K1t=option.preconditioner(t,pde,domain,option); % inv(K1)*t
+    pde.f=s;
+    K1s=option.preconditioner(s,pde,domain,option); % inv(K1)*s
+    
+    w=dot(K1t(:),K1s(:))/dot(K1t(:),K1t(:));
+    
+else
+    
+    z=s;
+    
+    t=option.operator(z,pde,domain);% A*s
+    w=dot(t(:),s(:))/dot(t(:),t(:));
+    
+end 
+    
+    v=v+alpha*y+w*z; % estimate of new solution
     
     % Recalculate every 100 iteration to remove roundoff errors
-    if mod(i,100)==0
+    if mod(i,20)==0
         
         r=f-option.operator(v,pde,domain);
         
     else
         
-        r=s-w*z;
+        r=s-w*t;%disp(rms(r(:)));
         
     end
     
     % Tolerance check
-    if rms(r)<option.tol
+    if rms(r(:))<option.tol
          
         fprintf('Bi Conjugate Gradient STAB Converged after %d iterations!\n',i);
         break
         
     end
-    
-%     % update for next guess
-%     % Check if preconditioner available
-%     if ~isempty(option.preconditioner) && option.prenumit~=0
-%     
-%         pde.f=r;
-%         option.numit=option.prenumit;
-%         s=option.preconditioner(zeros(size(v)),pde,domain,option);
-%     
-%     else
-%     
-%         s=r;
-%     
-%     end
      
     deltanew=dot(r(:),d(:));
     beta=(alpha/w)*deltanew/delta;
     p=r+beta*(p-w*q);
-    delta=deltanew;
+    
+%     % Check r.d =/= 0
+%     if abs(dot(r(:),d(:)))<option.tol
+% %         disp(abs(dot(r(:),d(:))))
+%         d=r;
+%         p=r;
+%     end
     
 end
 
