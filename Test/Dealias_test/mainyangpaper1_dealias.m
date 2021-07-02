9f5436c75f3c12bda653ea305c5b3c9e8ead3260
Keith Chan <kccha75@gmail.com>
2021-07-02 16:42:28 +1000

    more test files for dealias restrict ordering

---
+++ Test/Dealias_test/mainyangpaper1_dealias.m
@@ -0,0 +1,150 @@
+clear;close all
+% -------------------------------------------------------------------------
+% Solve Yang NLS using Newton's method
+% Note: there is a problem if coarsestgrid is 6 or lower 
+% 
+% -------------------------------------------------------------------------
+% INPUT PARAMETERS
+% -------------------------------------------------------------------------
+L(1) = 30;
+L(2) = 30;
+finestgrid = 8;
+coarsestgrid = 3;
+
+% PDE parameters
+
+a=@(X,Y) 1;
+b=@(X,Y) 1;
+
+mu=1;
+% c(x) function
+c=@(X,Y) mu;
+d=@(X,Y) -1;
+
+% RHS function
+f=@(X,Y) 0*X;
+
+% Initial guess
+v0=@(X,Y) 2.2*exp(-X.^2-Y.^2);
+
+% -------------------------------------------------------------------------
+% Set up parameters
+% -------------------------------------------------------------------------
+N(1) = 2^finestgrid;
+N(2) = 2^finestgrid;
+
+% Spectral Wave numbers
+k{1} = 2*pi/L(1)*[0:N(1)/2-1 -N(1)/2 -N(1)/2+1:-1]';
+k{2} = 2*pi/L(2)*[0:N(2)/2-1 -N(2)/2 -N(2)/2+1:-1]';
+[KX,KY] = ndgrid(k{1},k{2});
+
+x{1} = L(1)*(-N(1)/2:N(1)/2-1)'/N(1);
+x{2} = L(2)*(-N(2)/2:N(2)/2-1)'/N(2);
+[X,Y] = ndgrid(x{1},x{2});
+
+a=a(X,Y);
+b=b(X,Y);
+c=c(X,Y);
+d=d(X,Y);
+f=f(X,Y);
+
+v0=v0(X,Y);
+
+% -------------------------------------------------------------------------
+% Multigrid Options here
+% -------------------------------------------------------------------------
+
+% Number of V-cycles if option is chosen, otherwise number of v-cycles done
+% after FMG
+option.num_vcycles=0;
+
+% Solver / solution tolerance
+option.tol=1e-12;
+
+% Relaxations on the up and down cycle during Multigrid
+option.Nd=1;
+option.Nu=1;
+
+% Multigrid solver options:'V-cycle' or 'FMG'
+option.solver='FMG';
+
+% Multigrid scheme: 'Correction' or 'FAS'
+option.mgscheme='Correction';
+
+% Operator, coarse grid solver, Relaxation, Restriction, Prolongation options
+option.operator=@fourier_Lu_2d_mid;
+option.coarsegridsolver=@cg;
+option.relaxation=@MRR;
+option.restriction=@fourier_restrict_2d_mid;
+option.prolongation=@fourier_prolong_2d_mid;
+
+% Preconditioner
+option.preconditioner=@fourier_yang_pre_2d;
+% Number of precondition relaxations
+option.prenumit=1;
+
+% -------------------------------------------------------------------------
+% Sort into sctructures
+% -------------------------------------------------------------------------
+% Assuming constant dx
+dx(1) = x{1}(2)-x{1}(1);
+dx(2) = x{2}(2)-x{2}(1);
+
+% Sort into structures
+domain.L = L;
+domain.N = N;
+domain.k = k;
+domain.dx = dx;
+
+pde.a = a;
+pde.b = b;
+pde.c = c;
+pde.d = d;
+pde.f = f;
+
+option.finestgrid=finestgrid;
+option.coarsestgrid=coarsestgrid;
+option.grids=finestgrid-coarsestgrid+1;
+
+% -------------------------------------------------------------------------
+% NEWTON HERE
+% -------------------------------------------------------------------------
+option.jacobian_dealias=@jacobian_NLS_2d_dealias;
+v=v0;
+
+% Error guess (keep at 0)
+e0=zeros(N);
+
+tic
+for i=1:20
+    
+    % Jacobian fine grid here
+    jacobian=jacobian_NLS_2d(v,pde(1),domain(1));
+    
+    % Setup structures here for linear problem
+    [jacobian,domain]=setstructures(v,pde,jacobian,domain,option);
+    
+    r=rms(rms(jacobian(1).f));
+    fprintf('Residual Newton = %d\n',r)
+    if r<=1e-10
+        fprintf('Converged after %d Newton Iterations \n',i-1)
+        break
+    end
+    
+    % Solve linear equation
+%     option.tol=1e-1*r;
+%     [e,r]=cg(e0,jacobian,scheme,option);
+    [e,r]=mg_test_dealias(v,jacobian,domain,option);
+
+    % Update correction
+    v=v+e;
+
+    
+end
+
+if i==20
+    
+    fprintf('Did not converge to required tolerance after %d Newton Iterations\n',i)
+    
+end 
+toc
--- Test/test.m
+++ Test/test.m
@@ -1,3 +1,11 @@
+clear;
+
+x=linspace(-2*pi,2*pi,2^5);
+y=linspace(-2*pi,2*pi,2^5);
+
+[X,Y]=ndgrid(x,y);
+
+
 % Testing dealias effects ... and order of operation
 
 v1=sin(exp(-X.^2-Y.^2));
@@ -14,8 +22,16 @@ v2_dealias_c=fourier_restrict_2d_filtered(dealias_2d(v1,v1));
 % dealiased restricted?
 v2_c_dealias=dealias_2d(fourier_restrict_2d_filtered(v1),fourier_restrict_2d_filtered(v1));
 
-z=fourier_restrict_2d_filtered(v1).^2;
-
 figure;
 surf(v2_dealias_c-v2_c_dealias)
 % Quite different ...
+
+% Actual coarse grid ...
+x2=linspace(-2*pi,2*pi,2^4);
+y2=linspace(-2*pi,2*pi,2^4);
+
+[X2,Y2]=ndgrid(x2,y2);
+
+v1c=sin(exp(-X.^2-Y.^2));
+
+v2c=v1c.^2;
