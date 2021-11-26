734cb7e0810b8e9b829f5c026c8f624185d50a82
Keith Chan <kccha75@gmail.com>
2021-11-26 13:20:23 +1100

    added restrict and prolong without bcs

---
+++ Prolongation/cheb_prolong.m
@@ -0,0 +1,24 @@
+% Chebyshev Prolongation operator on x direction (first component)
+% assuming fine grid is 2x+1 coarse grid points in x
+%
+% Inputs:
+% vc - coarse grid (Nx * Ny * ... size)
+%
+% Outputs:
+% vc - coarse grid prolongation (Nx*2-1 * Ny * ... size)
+
+function vf=cheb_prolong(vc)
+
+N=size(vc);
+N(1)=N(1)-1;
+
+% Chebyshev transform
+vc_hat=fct(vc);
+
+% Filter high frequency and invert
+vf=ifct([vc_hat;zeros(N)]);
+
+% % Apply BCs
+% vf(1,:)=0;vf(end,:)=0;
+
+end
---
+++ Restriction/cheb_restrict.m
@@ -0,0 +1,30 @@
+% Chebyshev Restriction operator in the x direction only (first component)
+% assuming fine grid is 2x coarse grid points
+%
+% Inputs:
+% vf - fine grid (Nx * Ny * ... size)
+%
+% Outputs:
+% vc - coarse grid (ceil(Nx/2+1) * Ny * ... size)
+
+function vc=cheb_restrict(vf)
+
+N=size(vf);
+
+% Chebyshev transform
+v_hat=fct(vf);
+
+% For adjoint operator adjustment
+v_hat(ceil(N(1)/2),:)=v_hat(ceil(N(1)/2),:)/2;
+
+% Remove high frequency components and invert
+vc=ifct(v_hat(1:ceil(N(1)/2),:));
+
+% % Apply BCs (assume Dirichlet)
+% vc(1,:)=0;vc(end,:)=0;
+
+% Reshape to correct size (required for 3d or higher)
+N(1)=ceil(N(1)/2);
+vc=reshape(vc,N);
+
+end
