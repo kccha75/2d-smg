54dc30d4dcbeec47d2576ccd63e74200c736fa90
Keith Chan <kccha75@gmail.com>
2022-02-22 16:00:35 +1100

    added shooting method (bugs remaining)

---
+++ DJL/Eigenvalue/shootingmethod.m
@@ -0,0 +1,29 @@
+clear;clc;close all
+
+tspan=[0,1];
+
+c=linspace(-10000,10000,100);
+
+dy=linspace(-10,10,100);
+
+yend=zeros(length(c),length(dy));
+
+for i=1:length(c) % c loop
+    
+    for j=1:length(dy) % dy loop
+    
+        y0=[0;dy(j)];
+
+        ode=@(z,y) [y(2) ; -z/c(i)^2*y(1)];
+
+        [z,y]=ode45(ode,tspan,y0);
+    
+        yend(i,j)=y(end,1);
+        
+    end
+    
+end
+surf(c,dy,yend);
+xlabel('c');ylabel('dy')
+hold on
+surf(c,dy,zeros(length(c),length(dy)));
