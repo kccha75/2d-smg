4b9dc3b889b920f313acdabf80232d7e25690756
Keith Chan <kccha75@gmail.com>
2022-03-02 12:02:55 +1100

    added DJL main files + jacobian

--- DJL/jacobian_DJL.m
+++ DJL/jacobian_DJL.m
@@ -1,13 +1,13 @@
-% Gives the Jacobian for the PDE in the form
+% Gives the Jacobian for the DJL PDE in the form
 %
 % A_xx+A_zz+(z-A)*A/u^2=0
 %
-% Jacobian is e_xx+e_zz+(ze-2ev)/u^2
+% Jacobian is e_xx+e_zz+e(z-2v)/u^2
 %
 % Inputs:
-% pde.a
-% pde.b
-% pde.c
+% pde.a     coefficient in front of A_xx
+% pde.b     coefficient in front of A_zz
+% pde.c     coefficient in front of A
 % domain
 %
 % Ouputs:
@@ -16,10 +16,10 @@
 % jacobian.c
 
 function jacobian=jacobian_DJL(v,pde,domain)
-
+global u
 jacobian.a=pde.a;
 jacobian.b=pde.b;
-jacobian.c=???;
-jacobian.f=???;
+jacobian.c=pde.c-2*v/u^2;
+jacobian.f=pde.f-(Lu_2d(v,pde,domain)-v.^2/u^2);
 
 end
