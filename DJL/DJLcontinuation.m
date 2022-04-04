a068ce94b38525bb85d20d4ab1604609019b5524
Keith Chan <kccha75@gmail.com>
2022-04-04 11:42:37 +1000

    fixed bugs, more cleaning up to do

--- DJL/DJLcontinuation.m
+++ DJL/DJLcontinuation.m
@@ -1,16 +1,22 @@
+N=domain.N;
+x=domain.x;
+
 du=0.0001;
 
-global u
+u=pde.u;
+
 U(1)=u;
 V(1)=clenshaw_curtis2(2*trapint(v(1:N(1)/2+1,:).^2,x{1}(1:N(1)/2+1))'/pi*KAI*L/mu);
+
+converge=true;
 
 for ii=1:200
     fprintf('Step %d\n',ii)
     u=u+du;
-    
+    DJL.u=u;
     v0=v;
-    DJL_pde_initialise
-    DJLsolve
+    [pde,domain]=DJL_pde_initialise(DJL,domain);
+    v=DJLsolve(v0,pde,domain,option);
 
     if converge==false
         fprintf('OH NOOOO')
@@ -27,18 +33,3 @@ for ii=1:200
     end
 end
 
-plot(U,V)
-xlabel('u');ylabel('Momentum');title('mode 1 DJL')
-
-figure
-contour(X2,Y2,Y2-v,100)
-title("C=" + U(end))
-
-figure;
-plot(X2,Y2-v)
-title("C=" + U(end))
-
-% check dv<1 requirement
-dv=2*ifct(chebdiff(fct(v'),1));
-max(dv(:))
-min(dv(:))
