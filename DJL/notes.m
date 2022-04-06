5fee38f425fc2ae0a86ba0a9e9430d2fd3f7bd7e
Keith Chan <kccha75@gmail.com>
2022-04-06 13:01:25 +1000

    added notes to change next

---
+++ DJL/notes.m
@@ -0,0 +1,16 @@
+DJL.N=[N2,rho,jacobian];
+
+N2=@(z,xi) z-xi;
+rho= ...
+
+% jacobian function needs change
+jacobian.c=???
+jacobian.f=???
+
+% pde initialise needs change
+pde_initialise ...
+pde.c=???
+
+% v0 function for phi needs change also
+A=???
+% boundary conditions remain same
