51ef4578badec2087e92cf094cbd21c7dcd631a6
Keith Chan <kccha75@gmail.com>
2024-04-08 16:03:49 +1000

    yang fixes

--- Yang/Lu_kp.m
+++ Yang/Lu_kp.m
@@ -7,8 +7,9 @@
 function f=Lu_kp(v,pde,domain)
 
 k=domain.k;
+
 % f=real(ifft(-pde.a.*k{1}.^6.*fft(v)+pde.b.*k{1}.^4.*fft(v)-pde.c.*k{1}.^2.*fft(v)-k{2}.^2.*fft(v')));
 
 
-f=real(ifft2((-pde.a.*k{1}.^6+pde.b.*k{1}.^4-pde.c.*k{1}.^2).*fft(v)-(pde.d.*k{2}.^2.*fft(v'))'));
+f=real(ifft((-pde.a.*k{1}.^6+pde.b.*k{1}.^4-pde.c.*k{1}.^2).*fft(v)))-real(ifft(pde.d.*k{2}.^2.*fft(v'))');
 end
