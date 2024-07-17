226df9dc8683389b0477633f12cff8536ba196af
Keith Chan <kccha75@gmail.com>
2024-07-17 15:53:35 +1000

    chebtest progress

--- ChebTest.m
+++ ChebTest.m
@@ -18,20 +18,27 @@ kf=(0:Nf-1)';
 xf=cos(pi*kf/(Nf-1));
 
 % function
-f=tanh(x);
+f=sech(x).^2;
 
 % Dirichlet boundary
-f(1)=0;f(end)=0;
+f_dirich=f;
+f_dirich(1)=0;f_dirich(end)=0;
 
-plot(x,f)
+% plot(x,f)
 
 
 % Cheb restrict
-fc=cheb_restrict(f);
-figure;
-plot(xc,fc);
+fc=cheb_restrict(f_dirich);
+% figure;
+% plot(xc,fc);
 
 % Cheb prolong
-ff=cheb_prolong(f);
-figure;
-plot(xf,ff)
+ff=cheb_prolong(f_dirich);
+% figure;
+% plot(xf,ff)
+
+% Neumann
+
+% f(1)=
+a=-1/2*(-1)^(N-1)*x(1)-2*sum((-1).^(N-1+k(2:end-1))./(1+x(2:end-1)).*x(2:end-1))-(2*(N-1)^2+1)/6*x(end);
+b=(2*(N-1)^2+1)/6*x(1)+2*sum((-1).^k(2:end-1)./(1-x(2:end-1)))+1/2*(-1)^(N-1)
